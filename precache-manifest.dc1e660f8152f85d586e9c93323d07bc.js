757ab050cf79edcfb2ea36dd0cf55262de13f4d4
e-muravev <e.muravev.js@gmail.com>
2020-04-02 15:46:28 +0700

    Updates

---
+++ precache-manifest.dc1e660f8152f85d586e9c93323d07bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a968e431a7b75e0d68571a46091a9b",
+    "url": "/films-games-search-app/index.html"
+  },
+  {
+    "revision": "a3bbed70456725e0cfbf",
+    "url": "/films-games-search-app/static/css/main.d10f11a2.chunk.css"
+  },
+  {
+    "revision": "c3dff86c46fe9ee98f8a",
+    "url": "/films-games-search-app/static/js/2.0ae3d216.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/films-games-search-app/static/js/2.0ae3d216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3bbed70456725e0cfbf",
+    "url": "/films-games-search-app/static/js/main.a03cf291.chunk.js"
+  },
+  {
+    "revision": "1b6b3b7e5b9727ce9167",
+    "url": "/films-games-search-app/static/js/runtime-main.5a2f2898.js"
+  },
+  {
+    "revision": "731567aa1f48c055a1cd9d31ddfd74d9",
+    "url": "/films-games-search-app/static/media/default poster.731567aa.jpg"
+  }
+]);
